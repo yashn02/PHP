445032e4dbe6507699ecb70dea5dd6c6cb0cf9e9
yashn02 <yash.dhananjay-naktode@capgemini.com>
2025-08-15 15:58:02 +0530

    Add files via upload

---
+++ phppro/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Autos Index - Your Name</title>
+</head>
+<body>
+<h1>Welcome to the Autos Database</h1>
+<p><a href="login.php">Please log in</a></p>
+</body>
+</html>
